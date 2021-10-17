0f8b8bc30cb04561e41cd8b72121358da3294554
chiefmikey <wolfemikl@gmail.com>
2021-10-17 17:16:03 -0600

    Include mc-dns

---
+++ mc-dns/README.md
@@ -0,0 +1,8 @@
+## Ubuntu
+
+```sh
+sudo apt install -y wget
+sudo wget -O ~/mc-server-init.sh https://raw.githubusercontent.com/chiefmikey/scripts/main/mc-server/mc-server-init.sh
+sudo chmod +x ./mc-server-init.sh
+sudo ./mc-server-init.sh
+```
---
+++ mc-dns/mc-dns-init.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+INSTANCE_ALREADY_STARTED="INSTANCE_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e ~/$INSTANCE_ALREADY_STARTED ]; then
+sudo touch ~/$INSTANCE_ALREADY_STARTED
+  echo "-- First instance startup --"
+    sudo yum update -y
+    sudo yum install -y wget unzip git
+    sudo rm -rf /usr/local/go
+    wget https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
+    rm go1.17.2.linux-amd64.tar.gz
+    sudo echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+    source ~/.profile
+    git clone https://github.com/coredns/coredns
+    cd coredns
+    make
+    sudo echo \
+      ".:53 {
+        rewrite name exact mco.lbsg.net http://chaletlejar.com
+        forward . 1.1.1.1:53
+      }" \
+    >> ~/coredns/Corefile
+    sudo chmod +x ~/coredns/coredns
+    sudo ~/coredns/coredns
+else
+  echo "-- Not first instance startup --"
+    sudo ~/coredns/coredns
+fi
