ba7ff7465748b3aefda1990808dc9b20bbbbb5da
chiefmikey <wolfemikl@gmail.com>
2021-10-17 11:28:34 -0600

    Install wget and unzip

--- mc-server/mc-server-init.sh
+++ mc-server/mc-server-init.sh
@@ -6,6 +6,7 @@ sudo touch ~/$INSTANCE_ALREADY_STARTED
   echo "-- First instance startup --"
     sudo apt update -y
     sudo apt upgrade -y
+    sudo apt install -y wget unzip
     wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.34.02.zip
     unzip bedrock-server-1.17.34.02.zip
     rm bedrock-server-1.17.34.02.zip
