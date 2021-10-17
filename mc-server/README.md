1c241f529c393e01d59b9b9e1dd65c781b175fce
chiefmikey <wolfemikl@gmail.com>
2021-10-17 11:20:25 -0600

    Include mc-server

---
+++ mc-server/README.md
@@ -0,0 +1,8 @@
+## Ubuntu
+
+```sh
+apt install -y wget
+wget https://raw.githubusercontent.com/chiefmikey/codedeploy-ec2/main/codedeploy-ec2-ubuntu.sh
+chmod +x ./mc-server-init.sh
+sudo ./mc-server-init.sh
+```
---
+++ mc-server/mc-server-init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+INSTANCE_ALREADY_STARTED="INSTANCE_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e ~/$INSTANCE_ALREADY_STARTED ]; then
+sudo touch ~/$INSTANCE_ALREADY_STARTED
+  echo "-- First instance startup --"
+    sudo apt update -y
+    sudo apt upgrade -y
+    wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.34.02.zip
+    unzip bedrock-server-1.17.34.02.zip
+    rm bedrock-server-1.17.34.02.zip
+    LD_LIBRARY_PATH=. ./bedrock_server
+else
+  echo "-- Not first instance startup --"
+    LD_LIBRARY_PATH=. ./bedrock_server
+fi
