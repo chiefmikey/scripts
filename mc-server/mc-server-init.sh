038e99dcdd9931483ebf3a979ecd758f3518e955
chiefmikey <wolfemikl@gmail.com>
2021-10-17 12:27:05 -0600

    Update mc-server

--- mc-server/mc-server-init.sh
+++ mc-server/mc-server-init.sh
@@ -10,8 +10,14 @@ sudo touch ~/$INSTANCE_ALREADY_STARTED
     wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.34.02.zip
     unzip bedrock-server-1.17.34.02.zip
     rm bedrock-server-1.17.34.02.zip
+    sudo wget -O ~/server.properties https://raw.githubusercontent.com/chiefmikey/scripts/main/mc-server/server.properties
+    sudo wget -O ~/whitelist.json https://github.com/chiefmikey/scripts/blob/main/mc-server/whitelist.json
+    # sudo wget -O https://github.com/chiefmikey/scripts/blob/main/mc-server/permissions.json
     LD_LIBRARY_PATH=. ./bedrock_server
 else
   echo "-- Not first instance startup --"
+    sudo wget -O https://raw.githubusercontent.com/chiefmikey/scripts/main/mc-server/server.properties
+    sudo wget -O https://github.com/chiefmikey/scripts/blob/main/mc-server/whitelist.json
+    # sudo wget -O https://github.com/chiefmikey/scripts/blob/main/mc-server/permissions.json
     LD_LIBRARY_PATH=. ./bedrock_server
 fi
