0c59c6ed91e8cd633911d36f19ca17371f89ef27
chiefmikey <wolfemikl@gmail.com>
2022-03-01 13:29:34 -0700

    Update git auth

--- git/git-auth-ubuntu.sh
+++ git/git-auth-ubuntu.sh
@@ -7,6 +7,7 @@ apt install -y git jq awscli
 export user="chiefmikey"
 export email="wolfemikl@gmail.com"
 export repo="tales-from-the-script"
+export owner="chiefmikey"
 export awsRegion="us-east-2"
 export awsSecretId="git-auth"
 export pw=$(sudo aws secretsmanager --region ${awsRegion} get-secret-value --secret-id ${awsSecretId} | jq -r ".SecretString" | jq -r ".${awsSecretId}")
@@ -14,6 +15,6 @@ sleep 10
 git init
 git config user.name ${user}
 git config user.email ${email}
-git remote add origin https://${user}:${pw}@github.com/${user}/${repo}.git
+git remote add origin https://${user}:${pw}@github.com/${owner}/${repo}.git
 git fetch origin main
 git checkout main
