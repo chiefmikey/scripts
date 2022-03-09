f0ce4ce2b588438ad688868f69db3449ee8715c9
Mikl Wolfe <wolfemikl@gmail.com>
2022-03-09 08:00:56 -0700

    Update codedeploy-ubuntu.sh

--- codedeploy/codedeploy-ubuntu.sh
+++ codedeploy/codedeploy-ubuntu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install ruby-full -y
-sudo apt install wget -y
+apt update -y
+apt upgrade -y
+apt install ruby-full -y
+apt install wget -y
 CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
 $CODEDEPLOY_BIN stop
-sudo apt remove codedeploy-agent -y
+apt remove codedeploy-agent -y
 cd /home/ubuntu
 wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
 chmod +x ./install
-sudo ./install auto > /tmp/logfile
-sudo service codedeploy-agent status
+./install auto > /tmp/logfile
+service codedeploy-agent status
