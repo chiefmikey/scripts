cf380e63b3f9310a7338298b6f212f789f5b828b
Mikl Wolfe <wolfemikl@gmail.com>
2022-03-09 08:00:30 -0700

    Update codedeploy-amazon.sh

--- codedeploy/codedeploy-amazon.sh
+++ codedeploy/codedeploy-amazon.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo yum install -y ruby
-sudo yum install -y wget
+yum update -y
+yum install -y ruby
+yum install -y wget
 CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
 $CODEDEPLOY_BIN stop
-sudo yum erase codedeploy-agent -y
+yum erase codedeploy-agent -y
 cd /home/ec2-user
 wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
 chmod +x ./install
-sudo ./install auto
-sudo service codedeploy-agent status
+./install auto
+service codedeploy-agent status
