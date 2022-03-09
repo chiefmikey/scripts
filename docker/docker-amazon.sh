59f55f57a2634e13460a3626b4073c2d4d890c8f
Mikl Wolfe <wolfemikl@gmail.com>
2022-03-09 07:43:26 -0700

    Update docker-amazon.sh

--- docker/docker-amazon.sh
+++ docker/docker-amazon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
+yum update -y
+amazon-linux-extras install docker
+service docker start
+usermod -a -G docker ec2-user
+chkconfig docker on
