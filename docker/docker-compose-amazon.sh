f6532ad7df69061758af7058017eaad64a44e986
Mikl Wolfe <wolfemikl@gmail.com>
2022-03-09 07:44:16 -0700

    Update docker-compose-amazon.sh

--- docker/docker-compose-amazon.sh
+++ docker/docker-compose-amazon.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo yum install -y git wget
+yum update -y
+yum install -y git wget
 mkdir -p /home/ec2-user/.docker/cli-plugins/
 wget -O /home/ec2-user/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64
-sudo chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
-sudo ln -s /home/ec2-user/.docker/cli-plugins/docker-compose /usr/bin/docker-compose
-sudo amazon-linux-extras install docker
-sudo usermod -a -G docker $USER
-sudo su $USER
-sudo systemctl enable --now docker
-sudo chkconfig docker on
-sudo chmod 666 /var/run/docker.sock
+chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
+ln -s /home/ec2-user/.docker/cli-plugins/docker-compose /usr/bin/docker-compose
+amazon-linux-extras install docker
+usermod -a -G docker $USER
+su $USER
+systemctl enable --now docker
+chkconfig docker on
+chmod 666 /var/run/docker.sock
 docker compose -f /home/ec2-user/docker-compose.yaml pull
 docker compose -f /home/ec2-user/docker-compose.yaml up -d
