b296c7bf0b9ffee8a72a55ab4425d3937ca6a5cf
chiefmikey <wolfemikl@gmail.com>
2022-07-24 10:55:21 -0600

    Update homebrew autoupdate to stop and use start script

--- homebrew/update-homebrew.sh
+++ homebrew/update-homebrew.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 export SUDO_ASKPASS=$(${SCRIPT_DIR}/homebrew/ask-pass.sh)
-brew autoupdate delete
+brew autoupdate stop
 echo "+ brew update" && brew update
 echo "+ brew upgrade" && brew upgrade --greedy
 echo "+ brew cleanup" && brew cleanup
-brew autoupdate start ${1} --upgrade --cleanup --greedy --immediate
+${SCRIPT_DIR}/homebrew/update-homebrew-auto.sh ${1}
