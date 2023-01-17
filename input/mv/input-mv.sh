0a9512d78691b98a1d9cf736d91787ed4dce2888
Mikl Wolfe <wolfemikl@gmail.com>
2023-01-17 15:46:28 -0700

    update(input-mv): replace hard code string response to dynamic input path

--- input/mv/input-mv.sh
+++ input/mv/input-mv.sh
@@ -5,21 +5,25 @@ while getopts 's' OPTION; do
     s)
       export FLAG_CONFIRM="y"
       ;;
+    *)
+      echo "Usage: mv [-s] <file> <file>"
+      exit 1
+      ;;
   esac
 done
 shift "$((OPTIND-1))"
 
 if [ "${FLAG_CONFIRM}" = "y" ]; then
-  if [ -e ${1} ]; then
-  mv ${1} ${2}
-  echo "File is now e.eslintrc.js"
-  elif [ -e ${2} ]; then
-    echo "File is already e.eslintrc.js"
+  if [ -e "${1}" ]; then
+  mv "${1}" "${2}"
+  echo "File is now ${2}"
+  elif [ -e "${2}" ]; then
+    echo "File is already ${2}"
   fi
-elif [ -e ${1} ]; then
-  mv ${1} ${2}
-  echo "File is now e.eslintrc.js"
-elif [ -e ${2} ]; then
-  mv ${2} ${1}
-  echo "File is now .eslintrc.js"
+elif [ -e "${1}" ]; then
+  mv "${1}" "${2}"
+  echo "File is now ${2}"
+elif [ -e "${2}" ]; then
+  mv "${2}" "${1}"
+  echo "File is now ${1}"
 fi
