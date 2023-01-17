99ef24c2fb7e1bda0e6182f4a9533a16f145a49b
Mikl Wolfe <wolfemikl@gmail.com>
2023-01-16 20:30:18 -0700

    Disable npm next 9

--- npm/upgrade/npm-upgrade.sh
+++ npm/upgrade/npm-upgrade.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 echo "+ npm global upgrade"
-cd ${HOME}
+cd "${HOME}" || exit
 
 export STATUS=0
 for package in $(npm ls -g --json | jq '.dependencies | keys' | awk -F'"' '{print $2}'); do
-  export v1=$(${package} --version)
-  if [ "${package}" = "npm" ]; then
-    package="npm@next-9"
-  fi
-  if [ $(npm view ${package} version) != ${v1} ]; then
-     npm i -gs ${package}
+  v1="$(${package} --version)"
+  export v1
+  # if [ "${package}" = "npm" ]; then
+  #   package="npm@next-9"
+  # fi
+  if [ "$(npm view "${package}" version)" != "${v1}" ]; then
+     npm i -gs "${package}"
      if [ "${package}" = "npm" ]; then
        package="npm"
      fi
-     export v2=$(${package} --version)
+     v2="$(${package} --version)"
+     export v2
      echo "${package} (${v1} -> ${v2})"
      export STATUS=1
   fi;
