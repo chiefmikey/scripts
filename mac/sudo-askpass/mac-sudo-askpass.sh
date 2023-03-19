859f0611a08543df78a5c1cd183f54adcc0f2d34
Mikl Wolfe <wolfemikl@gmail.com>
2023-03-19 13:36:45 -0600

    feature(mac): update sudo pass condition

--- mac/sudo-askpass/mac-sudo-askpass.sh
+++ mac/sudo-askpass/mac-sudo-askpass.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 PW_NAME="SUDO_PASS"
-PW_ACCOUNT="Wolfe, Mikl"
+if [ "$(whoami)" = "mwolfe" ]; then
+  PW_ACCOUNT="Wolfe, Mikl"
+else
+  PW_ACCOUNT="Mikl Wolfe"
+fi
 
 if ! SUDO_PASS=$(security find-generic-password -w -s "${PW_NAME}" -a "${PW_ACCOUNT}"); then
   echo "Error: ${?}"
   # test to see what the cant find password error string is
-  if [ "${?##*something*}"]; then
+  if [ "${?##*something*}" ]; then
     echo "No password: ${PW_NAME} found for user: ${PW_ACCOUNT}"
     echo "Password:"
     read SUDO_PASS
