122f1b18252b3b06d82852e28fe2c462106b5efc
Mikl Wolfe <wolfemikl@gmail.com>
2023-04-18 09:15:54 -0600

    feature(input-command): escape variables

--- input/command/command.sh
+++ input/command/command.sh
@@ -4,15 +4,15 @@ while getopts 'c:d:f:' OPTION; do
   case $OPTION in
     c)
     # need to check if command is valid for shell
-      if [ -n ${OPTARG} ]; then
+      if [ -n "${OPTARG}" ]; then
         INPUT_COMMAND=${OPTARG}
       else
         echo "Invalid Command"
       fi
       ;;
     d)
-      if [ -n ${OPTARG} ]; then
-        if [ -d ${OPTARG} ]; then
+      if [ -n "${OPTARG}" ]; then
+        if [ -d "${OPTARG}" ]; then
           ROOT=${OPTARG}
         else
           echo "Invalid Directory"
@@ -23,7 +23,7 @@ while getopts 'c:d:f:' OPTION; do
       ;;
     f)
     # need to check if file exists
-      if [ -n ${OPTARG} ]; then
+      if [ -n "${OPTARG}" ]; then
         FILE=${OPTARG}
       else
         echo "Invalid File"
@@ -33,7 +33,7 @@ while getopts 'c:d:f:' OPTION; do
 done
 shift "$((OPTIND-1))"
 
-${INPUT_COMMAND} ${ROOT}/${FILE}
+${INPUT_COMMAND} "${ROOT}"/"${FILE}"
 
 DESC=${1}
-echo ${DESC}
+echo "${DESC}"
