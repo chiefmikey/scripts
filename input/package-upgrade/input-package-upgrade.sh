d571413a8b1d297965674b187ab583b314ef90c1
Mikl Wolfe <wolfemikl@gmail.com>
2023-09-05 13:18:34 -0600

    chore(formatting): clean up

--- input/package-upgrade/input-package-upgrade.sh
+++ input/package-upgrade/input-package-upgrade.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export USER=${1}
-export REPO=${2}
+export USER="${1}"
+export REPO="${2}"
 
 export CURRENT_VERSION=curl --silent "https://api.github.com/repos/${USER}/${REPO}/releases/latest" | jq ".. .tag_name? // empty"
 
-echo ${CURRENT_VERSION}
+echo "${CURRENT_VERSION}"
--- input/package-version/input-package-version.sh
+++ input/package-version/input-package-version.sh
@@ -4,13 +4,13 @@ export USER
 export REPO
 export URL="https://api.github.com/repos/${USER}/${REPO}/releases/latest"
 
-export NO_PACKAGE=$(curl --silent ${URL} | jq ".. .message? // empty")
+export NO_PACKAGE=$(curl --silent "${URL}" | jq ".. .message? // empty")
 
-if [ -z ${NO_PACKAGE} ]; then
-  export TAG_NAME=$(curl --silent ${URL} | jq ".. .tag_name? // empty")
-  export OFFLINE=$(if [ -z ${TAG_NAME} ]; then echo "true"; fi)
+if [ -z "${NO_PACKAGE}" ]; then
+  export TAG_NAME=$(curl --silent "${URL}" | jq ".. .tag_name? // empty")
+  export OFFLINE=$(if [ -z "${TAG_NAME}" ]; then echo "true"; fi)
 fi
 
-if [ -n ${NO_PACKAGE} ]; then echo "No package found for ${USER}/${REPO}"; fi
-if [ -n ${TAG_NAME} ]; then echo ${TAG_NAME}; fi
-if [ -n ${OFFLINE} ]; then echo "OFFLINE"; fi
+if [ -n "${NO_PACKAGE}" ]; then echo "No package found for ${USER}/${REPO}"; fi
+if [ -n "${TAG_NAME}" ]; then echo "${TAG_NAME}"; fi
+if [ -n "${OFFLINE}" ]; then echo "OFFLINE"; fi
