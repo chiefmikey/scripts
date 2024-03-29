da5e7a0df181d1962615e7a6cd0fde39b03441e5
Mikl Wolfe <wolfemikl@gmail.com>
2024-03-28 20:53:47 -0600

    feat(dock-sort): update

--- mac/dock-sort/mac-dock-sort.sh
+++ mac/dock-sort/mac-dock-sort.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "+ dock sort"
 cd "${HOME}" || exit
@@ -27,7 +27,6 @@ if [ "$(whoami)" = "mikl" ]; then
   defaults write com.apple.dock persistent-apps -array \
     "$(dock_item /Applications/Brave\ Browser.app)" \
     "$(dock_item /Applications/Google\ Chrome.app)" \
-    "$(dock_item /Applications/Google\ Chrome\ Canary.app)" \
     "$(dock_item /System/Applications/Messages.app)" \
     "$(dock_item /Applications/Spotify.app)" \
     "$(dock_item /System/Applications/Music.app)" \
@@ -35,13 +34,12 @@ if [ "$(whoami)" = "mikl" ]; then
     "$(dock_item /Applications/Logic\ Pro\ X.app)" \
     "$(dock_item /Applications/Reason\ Suite\ 11.app)" \
     "$(dock_item /Applications/Final\ Cut\ Pro.app)" \
-    "$(dock_item /Applications/Motion.app)" \
+    "$(dock_item /Applications/Apple\ Motion.app)" \
     "$(dock_item /Applications/Affinity\ Photo\ 2.app)" \
     "$(dock_item /Applications/Affinity\ Designer\ 2.app)" \
     "$(dock_item /System/Applications/Photos.app)" \
     "$(dock_item /Applications/iTerm.app)" \
     "$(dock_item /Applications/Visual\ Studio\ Code.app)" \
-    "$(dock_item /Applications/Visual\ Studio\ Code\ -\ Insiders.app)" \
     "$(dock_item /Applications/Docker.app)" \
     "$(dock_item /Applications/Postman.app)" \
     "$(dock_item /Applications/Parallels\ Desktop.app)" \
