7adf3f32bc65155d9be9534f802f2589bcdb133e
Mikl Wolfe <wolfemikl@gmail.com>
2024-05-29 16:17:18 -0600

    feat(mac-dock-sort): remove figma

--- mac/dock-sort/mac-dock-sort.sh
+++ mac/dock-sort/mac-dock-sort.sh
@@ -54,7 +54,6 @@ if [ "$(hostname)" = "QHQH24WCXG-mikl" ]; then
     "$(dock_item /Applications/Docker.app)" \
     "$(dock_item /Applications/Postman.app)" \
     "$(dock_item /Applications/Parallels\ Desktop.app)" \
-    "$(dock_item /Applications/Figma.app)" \
     "$(dock_item /Applications/Notion.app)" \
     "$(dock_item /Applications/ClickUp.app)" \
     "$(dock_item /Applications/zoom\.us.app)" \
